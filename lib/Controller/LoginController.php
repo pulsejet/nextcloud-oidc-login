19b3017a96dd79f45baf8695311530a2e06d0244
Varun Patil <radialapps@gmail.com>
2021-02-10 08:48:25 +0000

    Refactor oidc_create_groups

--- lib/Controller/LoginController.php
+++ lib/Controller/LoginController.php
@@ -317,12 +317,17 @@ class LoginController extends Controller
 
                 // Add user to group
                 foreach ($groupNames as $group) {
-                    if ($systemgroup = $this->groupManager->get($group)) {
+                    // Get existing group
+                    $systemgroup = $this->groupManager->get($group);
+
+                    // Create group if does not exist
+                    if (!$systemgroup && $this->config->getSystemValue('oidc_create_groups', false)) {
+                        $systemgroup = $this->groupManager->createGroup($group);
+                    }
+
+                    // Add user to group
+                    if ($systemgroup) {
                         $systemgroup->addUser($user);
-                    } else if($this->config->getSystemValue('oidc_create_groups', false) ){
-                        if($systemgroup = $this->groupManager->createGroup($group)) {
-                            $systemgroup->addUser($user);
-                        }
                     }
                 }
             }
