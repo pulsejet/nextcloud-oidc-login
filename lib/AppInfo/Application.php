4a14aedb79c2dc805ca16ca8d1ba0ed5e90de0ae
Avinash Gusain <avinashg@e.email>
2023-10-31 14:59:34 +0530

    formatting fix

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -20,159 +20,154 @@ use OCP\IUserSession;
 use OCP\Util;
 use OCA\OIDCLogin\WebDAV\BearerAuthBackend;
 
-class Application extends App implements IBootstrap
-{
-    protected IURLGenerator $url;
-    protected IL10N $l;
-    protected IConfig $config;
+class Application extends App implements IBootstrap {
+	protected IURLGenerator $url;
+	protected IL10N $l;
+	protected IConfig $config;
 
-    private $appName = 'oidc_login';
+	private $appName = 'oidc_login';
 
-    public function __construct()
-    {
-        parent::__construct($this->appName);
-    }
+	public function __construct() {
+		parent::__construct($this->appName);
+	}
 
-    public function register(IRegistrationContext $context): void
-    {
-        $context->registerAlternativeLogin(OIDCLoginOption::class);
+	public function register(IRegistrationContext $context): void {
+		$context->registerAlternativeLogin(OIDCLoginOption::class);
 
-        $context->registerEventListener(
-            'OCA\DAV\Connector\Sabre::authInit',
-            \OCA\OIDCLogin\WebDAV\BearerAuthBackend::class
-        );
+		$context->registerEventListener(
+			'OCA\DAV\Connector\Sabre::authInit',
+			\OCA\OIDCLogin\WebDAV\BearerAuthBackend::class
+		);
 
-        $context->registerEventListener(
-            'OCA\DAV\Connector\Sabre::addPlugin',
-            \OCA\OIDCLogin\WebDAV\BearerAuthBackend::class
-        );
+		$context->registerEventListener(
+			'OCA\DAV\Connector\Sabre::addPlugin',
+			\OCA\OIDCLogin\WebDAV\BearerAuthBackend::class
+		);
 
-        $context->registerEventListener(
-            'OCA\DAV\Connector\Sabre::authInit',
-            \OCA\OIDCLogin\WebDAV\BasicAuthBackend::class
-        );
+		$context->registerEventListener(
+			'OCA\DAV\Connector\Sabre::authInit',
+			\OCA\OIDCLogin\WebDAV\BasicAuthBackend::class
+		);
 
-        $context->registerEventListener(
-            'OCA\DAV\Connector\Sabre::addPlugin',
-            \OCA\OIDCLogin\WebDAV\BasicAuthBackend::class
-        );
-    }
+		$context->registerEventListener(
+			'OCA\DAV\Connector\Sabre::addPlugin',
+			\OCA\OIDCLogin\WebDAV\BasicAuthBackend::class
+		);
+	}
 
-    public function boot(IBootContext $context): void
-    {
-        $container = $context->getAppContainer();
-        $this->l = $container->get(IL10N::class);
-        $this->url = $container->get(IURLGenerator::class);
-        $this->config = $container->get(IConfig::class);
+	public function boot(IBootContext $context): void {
+		$container = $context->getAppContainer();
+		$this->l = $container->get(IL10N::class);
+		$this->url = $container->get(IURLGenerator::class);
+		$this->config = $container->get(IConfig::class);
 
-        /** @var IRequest */
-        $request = $container->get(IRequest::class);
-        $bearerAuthBackend = $container->query(BearerAuthBackend::class);
+		/** @var IRequest */
+		$request = $container->get(IRequest::class);
+		$bearerAuthBackend = $container->query(BearerAuthBackend::class);
 
-        // If it is an OCS request, try to authenticate with bearer token
-        if ($request->getHeader('OCS-APIREQUEST') === 'true' &&
-        $request->getHeader('OIDC-LOGIN-WITH-TOKEN') === 'true' &&
-        str_starts_with($request->getHeader('Authorization'), 'Bearer ')) {
-            $this->loginWithBearerToken($request, $bearerAuthBackend);
-        }
+		// If it is an OCS request, try to authenticate with bearer token
+		if ($request->getHeader('OCS-APIREQUEST') === 'true' &&
+		$request->getHeader('OIDC-LOGIN-WITH-TOKEN') === 'true' &&
+		str_starts_with($request->getHeader('Authorization'), 'Bearer ')) {
+			$this->loginWithBearerToken($request, $bearerAuthBackend);
+		}
 
 
-        // Check if automatic redirection is enabled
-        $useLoginRedirect = $this->config->getSystemValue('oidc_login_auto_redirect', false);
+		// Check if automatic redirection is enabled
+		$useLoginRedirect = $this->config->getSystemValue('oidc_login_auto_redirect', false);
 
-        // Check if alternative login page is enabled
-        $altLoginPage = $this->config->getSystemValue('oidc_login_alt_login_page', false);
+		// Check if alternative login page is enabled
+		$altLoginPage = $this->config->getSystemValue('oidc_login_alt_login_page', false);
 
-        // URL for login without redirecting forcefully, false if we are not doing that
-        $noRedirLoginUrl = $useLoginRedirect ? $this->url->linkToRouteAbsolute('core.login.showLoginForm') . '?noredir=1' : false;
+		// URL for login without redirecting forcefully, false if we are not doing that
+		$noRedirLoginUrl = $useLoginRedirect ? $this->url->linkToRouteAbsolute('core.login.showLoginForm') . '?noredir=1' : false;
 
-        // Get logged in user's session
-        $userSession = $container->get(IUserSession::class);
-        $session = $container->get(ISession::class);
+		// Get logged in user's session
+		$userSession = $container->get(IUserSession::class);
+		$session = $container->get(ISession::class);
 
-        // Check if the user is logged in
-        if ($userSession->isLoggedIn()) {
-            // Halt processing if not logged in with OIDC
-            if (!$session->exists('is_oidc')) {
-                return;
-            }
+		// Check if the user is logged in
+		if ($userSession->isLoggedIn()) {
+			// Halt processing if not logged in with OIDC
+			if (!$session->exists('is_oidc')) {
+				return;
+			}
 
-            // Disable password confirmation for user
-            $session->set('last-password-confirm', $container->get(ITimeFactory::class)->getTime());
+			// Disable password confirmation for user
+			$session->set('last-password-confirm', $container->get(ITimeFactory::class)->getTime());
 
-            /* Redirect to logout URL on completing logout
-               If do not have logout URL, go to noredir on logout */
-            if ($logoutUrl = $session->get('oidc_logout_url', $noRedirLoginUrl)) {
-                $userSession->listen('\OC\User', 'postLogout', function () use ($logoutUrl, $session) {
-                    // Do nothing if this is a CORS request
-                    if ($this->getContainer()->get(ControllerMethodReflector::class)->hasAnnotation('CORS')) {
-                        return;
-                    }
+			/* Redirect to logout URL on completing logout
+			   If do not have logout URL, go to noredir on logout */
+			if ($logoutUrl = $session->get('oidc_logout_url', $noRedirLoginUrl)) {
+				$userSession->listen('\OC\User', 'postLogout', function () use ($logoutUrl, $session) {
+					// Do nothing if this is a CORS request
+					if ($this->getContainer()->get(ControllerMethodReflector::class)->hasAnnotation('CORS')) {
+						return;
+					}
 
-                    // Properly close the session and clear the browsers storage data before
-                    // redirecting to the logout url.
-                    $session->set('clearingExecutionContexts', '1');
-                    $session->close();
-                    header('Clear-Site-Data: "cache", "storage"');
+					// Properly close the session and clear the browsers storage data before
+					// redirecting to the logout url.
+					$session->set('clearingExecutionContexts', '1');
+					$session->close();
+					header('Clear-Site-Data: "cache", "storage"');
 
-                    header('Location: ' . $logoutUrl);
+					header('Location: ' . $logoutUrl);
 
-                    exit;
-                });
-            }
+					exit;
+				});
+			}
 
-            // Hide password change form
-            if ($this->config->getSystemValue('oidc_login_hide_password_form', false)) {
-                Util::addStyle($this->appName, 'oidc.hidepasswordform');
-            }
+			// Hide password change form
+			if ($this->config->getSystemValue('oidc_login_hide_password_form', false)) {
+				Util::addStyle($this->appName, 'oidc.hidepasswordform');
+			}
 
-            return;
-        }
+			return;
+		}
 
-        // Redirect automatically or show alt login page
-        if (\array_key_exists('REQUEST_METHOD', $_SERVER)
-            && 'GET' === $_SERVER['REQUEST_METHOD']
-            && '/login' === $request->getPathInfo()
-            && null === $request->getParam('noredir')
-            && null === $request->getParam('user')
-        ) {
-            // Set redirection URL
-            $redir = $request->getParam('redirect_url');
-            if (null !== $redir && !empty($redir)) {
-                $session->set('oidc_redir', $redir);
-            } else {
-                $session->set('oidc_redir', '/');
-            }
+		// Redirect automatically or show alt login page
+		if (\array_key_exists('REQUEST_METHOD', $_SERVER)
+			&& 'GET' === $_SERVER['REQUEST_METHOD']
+			&& '/login' === $request->getPathInfo()
+			&& null === $request->getParam('noredir')
+			&& null === $request->getParam('user')
+		) {
+			// Set redirection URL
+			$redir = $request->getParam('redirect_url');
+			if (null !== $redir && !empty($redir)) {
+				$session->set('oidc_redir', $redir);
+			} else {
+				$session->set('oidc_redir', '/');
+			}
 
-            // Get URLs
-            $loginLink = OIDCLoginOption::getLoginLink($request, $this->url);
+			// Get URLs
+			$loginLink = OIDCLoginOption::getLoginLink($request, $this->url);
 
-            // Force redirect
-            if ($useLoginRedirect) {
-                header('Location: ' . $loginLink);
+			// Force redirect
+			if ($useLoginRedirect) {
+				header('Location: ' . $loginLink);
 
-                exit;
-            }
+				exit;
+			}
 
-            // Alt login page
-            if ($altLoginPage) {
-                $OIDC_LOGIN_URL = $loginLink; // available in alt login page
-                header_remove('content-security-policy');
+			// Alt login page
+			if ($altLoginPage) {
+				$OIDC_LOGIN_URL = $loginLink; // available in alt login page
+				header_remove('content-security-policy');
 
-                require $altLoginPage;
+				require $altLoginPage;
 
-                exit;
-            }
-        }
-    }
+				exit;
+			}
+		}
+	}
 
-    private function loginWithBearerToken(IRequest $request, BearerAuthBackend $bearerAuthBackend)
-    {
-        $authHeader = $request->getHeader('Authorization');
-        $bearerToken = substr($authHeader, 7);
-        if (empty($bearerToken)) {
-            return;
-        }
-        $bearerAuthBackend->validateBearerToken($bearerToken);
-    }
+	private function loginWithBearerToken(IRequest $request, BearerAuthBackend $bearerAuthBackend) {
+		$authHeader = $request->getHeader('Authorization');
+		$bearerToken = substr($authHeader, 7);
+		if (empty($bearerToken)) {
+			return;
+		}
+		$bearerAuthBackend->validateBearerToken($bearerToken);
+	}
 }
