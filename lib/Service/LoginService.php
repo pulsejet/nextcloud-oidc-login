e2f9d8eb2ea37f7f9de07075bc83f7110d69d72c
mathsyx69 <65737224+mathsyx69@users.noreply.github.com>
2024-03-30 14:00:53 +0100

    Update LoginService.php
    Resolve :  Issue : password create in nextcloud database for OpenId connect users #207

--- lib/Service/LoginService.php
+++ lib/Service/LoginService.php
@@ -134,8 +134,9 @@ class LoginService
 
         // Create user if not existing
         if (null === $user) {
-            // Generate random password of 30 characters
-            $password = substr(base64_encode(random_bytes(64)), 0, 30);
+            // Generate random password of 30 characters that meets nextcloud's strictest password policy
+            $randomPassword = substr(base64_encode(random_bytes(64)), 0, 28);
+            $password = "aA".$randomPassword."1$";
 
             // Create user. This will throw if creation is not permitted.
             $user = $this->createUser($uid, $password);
